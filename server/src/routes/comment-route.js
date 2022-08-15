db142ec424d20407d3e7f2b5ecf1768761820aee
kpzzy <sdjjm95@naver.com>
2022-08-15 14:09:50 +0900

    1111

--- server/src/routes/comment-route.js
+++ server/src/routes/comment-route.js
@@ -1,10 +1,7 @@
 import express from "express";
-<<<<<<< HEAD
+
 import * as controllertest from "../controller/comment-controller.js";
 
-=======
-import * as controllertest from "../controller/comment-controller.js"
->>>>>>> 468a19a4090d0ee5f16dd20d1bddecb448c8d436
 import { LoginCheck } from "../middlewares/loginMiddleware.js";
 
 const router = express.Router();
@@ -13,11 +10,11 @@ router.post('/create', LoginCheck, controllertest.commentCreate);
 router.put('/:commentId', LoginCheck , controllertest.commentUpdate);
 router.delete('/:commentId', LoginCheck, controllertest.commentDelete);
 router.get('/test/:commentId', LoginCheck , controllertest.commentFind);
-<<<<<<< HEAD
 
-=======
+
+
 // 얘는 내가 테스트하려고 만든 곳
 // 
->>>>>>> 468a19a4090d0ee5f16dd20d1bddecb448c8d436
+
 export default router;
 
