b55f1b7093d5343dd068d0642a1fbba4bfadfe09
kpzzy <sdjjm95@naver.com>
2022-08-17 21:18:33 +0900

    1분 토큰 만료

--- server/src/controller/comment-controller.js
+++ server/src/controller/comment-controller.js
@@ -4,7 +4,7 @@ import Post from "../models/post.js";
 
 const commentCreate = (async (req, res, next) => {
   const userId = req.userId;
-  console.log(req.userId)
+  // console.log(req.userId)
   const postId = req.params.postId;
   const { comment } = req.body;
 
@@ -14,6 +14,9 @@ const commentCreate = (async (req, res, next) => {
         PostId : postId,
         comment,
       });
+      // const formData = new FormData();
+      // formData.forEach((value, key) => comment1[key] = value);
+      // console.log(comment1)
       return res.status(200).json ({ 
         success: true,
         message: "댓글 생성",
@@ -33,12 +36,13 @@ const commentCreate = (async (req, res, next) => {
     
     try{
       const comment1 = await Comment.findOne({ where: {id: commentId} });
-      // console.log(comment1)
+      console.log(comment1.UserId)
       if(userId !== comment1.UserId) 
-      return res.send('나의 댓글이 아닙니다.')
+      return res.status(400).send('나의 댓글이 아닙니다.')
       const update = await Comment.update({
         comment,
       }, { where: {id : commentId} });
+      // console.log(update)
       return res.status(200).json ({
         success: true,
         message: "댓글 수정",
@@ -53,16 +57,21 @@ const commentCreate = (async (req, res, next) => {
   const commentDelete = (async (req, res, next) => {
     const userId = req.userId;
     const commentId = req.params.commentId;
+    // console.log(userId)
     try{
       const comment1 = await Comment.findOne({ where : { id: commentId} });
-      if(userId !== comment1.UserId) 
-      return res.status(200).send('나의 댓글이 아닙니다.')
-      if(!comment1)
-      return res.status(400).send('없는 댓글입니다.')
+      console.log(comment1)
+      if(userId !== comment1.UserId){
+        return res.status(400).json({
+          success : false,
+          message : '나의 댓글이 아닙니다',
+          result : {comment1}
+        })
+      }     
       const destroy = await Comment.destroy({
         where: {id: commentId}
       })
-      return res.json ({
+      return res.status(200).json ({
         success: true,
         message: "댓글 삭제",
         result: {comment1}
